cda065ff44a1d60f9d5f8b08fc55a08dd621e72e
ChrisCatto <chriscatto08@gmail.com>
2024-08-29 22:45:55 +0700

    mt6768-common: Link mediatek camera provider against v32 libutils * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0   which isn't already owned' * Required after U QPR3
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>
    Signed-off-by: nadinsylaa <asepmlnaaa@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,7 +76,8 @@ function blob_fixup {
         vendor/lib64/libgf_hal.so)
             sed -i 's|\x00\x14\xa0\x83_\xb8\xfd{C\xa9\xff\x03\x01\x91\xc0\x03_\xd6\xff\x83\x01\xd1\xfd{\x05\xa9\xfdC\x01\x91|\x00\x14\xa0\x83_\xb8\xfd{C\xa9\xff\x03\x01\x91\xc0\x03_\xd6\x00\x00\xe0\xd2\xc0\x03_\xd6\xfdC\x01\x91|g' "${2}"
             ;;
-        vendor/lib64/libmtkcam_stdutils.so)
+        vendor/lib*/libmtkcam_stdutils.so|\
+        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib*/libaalservice.so|\
